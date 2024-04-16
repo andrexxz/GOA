888b2d843a156acf7e371f204cc36bee169b46d2
andrexxz <j03475378@gmail.com>
2024-04-16 20:39:44 +0400

    commit

---
+++ Day 22/classwork/lesson22.py
@@ -0,0 +1,7 @@
+i = "goa"
+while input("who is the best academy?:  ") != i:
+    input("who is the best academy?:  ")
+
+else:
+    print("your answer is right")
+
---
+++ Day 22/homework/lesson22.py
@@ -0,0 +1,6 @@
+i = "mr nika keshelava"
+while input("who is the goal oriented academy owner?:  ") != i:
+    input("who is the goal oriented academy owner?:  ")
+
+else:
+    print("you are right! goal oriented academy owner is " + str(i))
