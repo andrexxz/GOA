4720e5f3278db468a15616bcc0c1f62f732eaf12
andrexxz <j03475378@gmail.com>
2024-04-19 19:58:52 +0400

    commit

--- Day 22/homework/lesson22.py
+++ Day 22/homework/lesson22.py
@@ -1,6 +1,7 @@
-i = "mr nika keshelava"
-while input("who is the goal oriented academy owner?:  ") != i:
-    input("who is the goal oriented academy owner?:  ")
+owner = "mr nika keshelava"
+question = input("who is the goal oriented academy owner?: ")
+while question != owner:
+    question = input("who is the goal oriented academy owner?:  ")
 
-else:
-    print("you are right! goal oriented academy owner is " + str(i))
+
+print("you are right! goal oriented academy owner is " + owner)
