f8aafdd6a365b4241c984b2f737de233d486aeaa
andrexxz <j03475378@gmail.com>
2024-07-08 19:11:50 +0400

    commit

--- Day 44/homework/lesson44.py
+++ Day 44/homework/lesson44.py
@@ -17,13 +17,10 @@ for i in range (0,100):
     else:
         print("the odd is " + str(i))    
 #4) Enter a number to the user and then using a for loop output the sum of all the numbers up to this number (for example, if he enters 10, output the sum of all the numbers up to 10, for example).
-for i in range (10):
-    number = int(input("შეიყვანეთ რიცხვი: "))
-
 sum_of_numbers = 0
-for i in range(1, number + 1):
+number = int(input("შეიყვანეთ რიცხვი: "))
+for i in range(number):
     sum_of_numbers += i
-
 print("რიცხვების ჯამი: " + str(sum_of_numbers))
 
 #5) Write an algorithm that prints multiples of 5 (numbers divisible by 5) from 1 to 50 inclusive
@@ -36,20 +33,19 @@ for i in range(1, 51):
 
 #1) Print even numbers up to 20.
 
-num = 1
+num = 0
 while num <= 20:
     if num % 2 == 0:
         print(num)
     num += 1
-
 #2) calculate the sum of numbers from 1 to 10.
 
-num = 1
+i = 0
 total_sum = 0
 
-while num <= 10:
-    total_sum += num
-    num += 1
+while i <= 10:  
+    total_sum += i
+    i += 1
 
 print("The sum of numbers from 1 to 10 is:" + str(total_sum))
 
@@ -60,8 +56,8 @@ question = int(input("enter number from 1 to 10:  "))
 while question != correct_answer:
     print("try again")
     question = int(input("enter number from 1 to 10:  "))
-else:
-    print("you gess the number finally")
+print("you gess the number finally")
+
 #4) Write a while loop that processes a list of numbers, doubling each number, and prints the new list.
 
 numbers = [1, 2, 3, 4, 5]
