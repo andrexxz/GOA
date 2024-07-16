376e0d238e0f898b91f0364316ab9be34fa90887
andrexxz <j03475378@gmail.com>
2024-07-16 18:53:16 +0400

    commit

---
+++ Day 48/homework/lesson48.py
@@ -0,0 +1,26 @@
+#1)
+u = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range (len(u)):
+    print(u(i))
+#2)
+q = []
+
+for i in range (1000):
+    qu = input("please type the thing:  ")
+    q.append(qu)
+
+#3)
+w = [] 
+e = [] 
+r = []
+for i in range (w,e,r):
+    print(w,e,r)  
+
+#4)
+name = "andria"
+female = "korchilava"
+both = []
+
+both.append(name,female)
+print("both")
