46305f351f1736acb6b35e7b1d9f7994980e5e5e
andrexxz <korchilavaandria@gmail.com>
2025-01-18 13:20:54 +0400

    commit

---
+++ Day 86/classwork/lesson86.js
@@ -0,0 +1,2 @@
+/*Level 86:
+https://stanford.edu/~cpiech/karel/ide.html*/
---
+++ Day 86/homework/lesson86.js
@@ -0,0 +1,9 @@
+function checkNumberSign(number) {
+    if (number > 0) {
+        console.log("რიცხვი არის დადებითი");
+    } else if (number < 0) {
+        console.log("რიცხვი არის უარყოფითი");
+    } else {
+        console.log("რიცხვი არის ნულის ტოლი");
+    }
+}
